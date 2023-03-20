1f6a208a7c2319de850e4b92b2f150d995be57da
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-03-19 22:43:24 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json", 
+            "Accept": "application/json"
+          },
+        body: JSON.stringify({
+          name, email 
+        }),
+       
+      })
+      .then((response)=> response.json())
+      .then((data)=>{
+        console.log(data)
+        document.body.innerHTML = data.id
+    })
+    .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        document.body.innerHTML = error.message
+        console.log(error.message);
+      });
+
+
+}
